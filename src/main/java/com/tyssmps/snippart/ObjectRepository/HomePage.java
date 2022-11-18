59c7339683977e68d629d0395fdec2d4120e6c44
PraveenBalaraj98 <108871724+PraveenBalaraj98@users.noreply.github.com>
2022-11-18 13:07:16 +0530

    Update HomePage.java

--- src/main/java/com/tyssmps/snippart/ObjectRepository/HomePage.java
+++ src/main/java/com/tyssmps/snippart/ObjectRepository/HomePage.java
@@ -1,9 +1,3 @@
 package com.tyssmps.snippart.ObjectRepository;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
 public class HomePage {
 }
