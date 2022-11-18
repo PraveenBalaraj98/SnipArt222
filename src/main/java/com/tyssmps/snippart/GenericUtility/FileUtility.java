1ef6a74403a4d70bd6411cb27c3c6230b92a67a0
Dell <Dell@DESKTOP-BOQM7D0>
2022-11-18 13:26:18 +0530

    Praveen-1811220125

--- src/main/java/com/tyssmps/snippart/GenericUtility/FileUtility.java
+++ src/main/java/com/tyssmps/snippart/GenericUtility/FileUtility.java
@@ -1,5 +1,21 @@
 package com.tyssmps.snippart.GenericUtility;
 
-public class FileUtility {
+import java.io.FileInputStream;
+import java.util.Properties;
 
+
+/**
+ * @author Praveen
+ *
+ */
+public class FileUtility {
+	public String getPropertyKeyValue(String key) throws Throwable {
+		
+		FileInputStream fis = new FileInputStream(IConstants.filepath);
+		Properties pObj = new Properties();
+		pObj.load(fis);
+		String value = pObj.getProperty(key);
+		return value;
+		
+	}
 }
--- src/main/java/com/tyssmps/snippart/GenericUtility/JavaUtility.java
+++ src/main/java/com/tyssmps/snippart/GenericUtility/JavaUtility.java
@@ -1,5 +1,80 @@
+
+
+
 package com.tyssmps.snippart.GenericUtility;
 
+import java.util.Date;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+/**
+ * @author Praveen
+ * @since 1998
+ *
+ */
 public class JavaUtility {
 
+	
+	public String generateRandomNumber(int range){
+
+		Random ran=new Random();
+		int num = ran.nextInt(range);
+		String number = Integer.toString(num);
+		return number;
+
+	}
+
+	public String  getSystemDate(String format) {
+		String Date="";
+
+		if(format.equalsIgnoreCase("YYYYMMDD")) {
+			Date date = new Date();
+			String currentDate = date.toString();
+			System.out.println(currentDate);
+			String[] arr = currentDate.split(" ");
+
+			String yyyy = arr[5];
+			String dd = arr[2];
+			int mm = date.getMonth()+1;
+
+			Date = yyyy+"-"+mm+"-"+dd;
+			
+		}
+		/**
+		 *  its used to get the current system date based on DD-MM-YYYY format 
+		 * @return
+		 */
+		if(format.equalsIgnoreCase("DDMMYYYY")) {
+			Date date = new Date();
+			String currentDate = date.toString();
+			System.out.println(currentDate);
+			String[] arr = currentDate.split(" ");
+
+			String yyyy = arr[5];
+			String dd = arr[2];
+			int mm = date.getMonth()+1;
+
+			Date = dd+"-"+mm+"-"+yyyy;
+		
+		}
+		/**
+		 *  its used to get the current system date based on MM-DD-YYYY formate
+		 * @return
+		 */
+		if(format.equalsIgnoreCase("MMDDYYYY")) {
+			Date date = new Date(0);
+			String currentDate = date.toString();
+			System.out.println(currentDate);
+			String[] arr = currentDate.split(" ");
+
+			String yyyy = arr[5];
+			String dd = arr[2];
+			int mm = date.getMonth()+1;
+
+			Date = mm+"-"+dd+"-"+yyyy;
+			
+		}
+		return Date;
+
+	}
 }
